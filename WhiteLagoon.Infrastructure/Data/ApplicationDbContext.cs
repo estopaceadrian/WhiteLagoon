19b3477f58e891512c2bbb39b28975f9e06d12f3
adrian <estopaceadrian@gmail.com>
2023-12-20 21:48:26 +0800

    created data for db

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -4,11 +4,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Infrastructure.Data 
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Description = "Royal Villa Description",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    Sqft = 550
+                },
+                new Villa
+                {
+                    Id = 2,
+                    Name = "Royal Villa II",
+                    Description = "Royal Villa II Description",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    Sqft = 550
+                },
+                new Villa
+                {
+                    Id = 3,
+                    Name = "Royal Villa III",
+                    Description = "Royal Villa III Description",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    Sqft = 550
+                },
+                new Villa
+                {
+                    Id = 4,
+                    Name = "Royal Villa IV",
+                    Description = "Royal Villa IV Description",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    Sqft = 550
+                }
+            );
+        }
     }
 }
---
+++ WhiteLagoon.Infrastructure/Migrations/20231220093135_seedVillaToDb.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedVillaToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, null, "Royal Villa Description", "https://placehold.co/600x400", "Royal Villa", 4, 200.0, 550, null },
+                    { 2, null, "Royal Villa II Description", "https://placehold.co/600x400", "Royal Villa II", 4, 200.0, 550, null },
+                    { 3, null, "Royal Villa III Description", "https://placehold.co/600x400", "Royal Villa III", 4, 200.0, 550, null },
+                    { 4, null, "Royal Villa IV Description", "https://placehold.co/600x400", "Royal Villa IV", 4, 200.0, 550, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
