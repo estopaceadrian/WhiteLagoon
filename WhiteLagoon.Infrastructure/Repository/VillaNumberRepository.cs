5cd94f36b4a2b0ddaeb3feacbe57a58ada56ff92
adrian <estopaceadrian@gmail.com>
2023-12-29 16:12:49 +0800

    implement dynamic villa number repository

--- WhiteLagoon.Infrastructure/Repository/VillaNumberRepository.cs
+++ WhiteLagoon.Infrastructure/Repository/VillaNumberRepository.cs
@@ -4,21 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Infrastructure.Repository
 {
-    public class UnitOfWork : IUnitOfWork
+    public class VillaNumberRepository : Repository<VillaNumber>, IVillaNumberRepository
     {
         private readonly ApplicationDbContext _db;
-        public IVillaRepository Villa { get; private set; }
-
-        public IVillaNumberRepository VillaNumber { get; private set; }
-        public UnitOfWork(ApplicationDbContext db)
+        public VillaNumberRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
-            Villa = new VillaRepository(_db);
-            VillaNumber = new VillaNumberRepository(_db);
+        }
+        public void Update(VillaNumber entity)
+        {
+            _db.VillaNumbers.Update(entity);
         }
     }
 }
