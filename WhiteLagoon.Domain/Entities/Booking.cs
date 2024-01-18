07a9e878a7b0c44b7712550f9f24b525f6e8ac26
adrian <estopaceadrian@gmail.com>
2024-01-18 20:23:27 +0800

    create bookings

---
+++ WhiteLagoon.Domain/Entities/Booking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        public int VillaId { get; set; }
+
+        [ForeignKey("VillaId")]
+        public Villa Villa { get; set;}
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
