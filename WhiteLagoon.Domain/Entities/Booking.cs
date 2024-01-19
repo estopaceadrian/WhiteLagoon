ffa6ccd9e8ba7b515ae9f54828c4bab039dc17a1
adrian <estopaceadrian@gmail.com>
2024-01-19 23:05:37 +0800

    update booking model

--- WhiteLagoon.Domain/Entities/Booking.cs
+++ WhiteLagoon.Domain/Entities/Booking.cs
@@ -28,7 +28,25 @@ namespace WhiteLagoon.Domain.Entities
         public string Name { get; set; }
         [Required]
         public string Email { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
+
+        [Required]
+        public double TotalCost { get; set; }
+        public int Nights { get; set; }
+        public string? Status { get; set; }
+
+        [Required]
+        public DateTime BookingDate { get; set; }
+        [Required]
+        public DateOnly CheckInDate { get; set; }
+        [Required]
+        public DateOnly CheckOutDate { get; set; }
+
+        public bool IsPaymentSuccessfull { get; set; } = false;
+        public DateTime PaymentDate { get; set; }
+
+        public string? StripeSessionId { get; set; }
+        public string? StripePaymentIntentId { get; set; }
 
     }
 }
