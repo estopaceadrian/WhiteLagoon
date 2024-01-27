0433f2e743a3396362d444482c0ea41d371ad517
adrian <estopaceadrian@gmail.com>
2024-01-27 10:11:32 +0800

    create villa rooms available count:

--- WhiteLagoon.Application/Common/Utility/StaticDetail.cs
+++ WhiteLagoon.Application/Common/Utility/StaticDetail.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
 
 namespace WhiteLagoon.Application.Common.Utility
 {
@@ -12,10 +13,48 @@ namespace WhiteLagoon.Application.Common.Utility
         public const string Role_Admin = "Admin";
 
         public const string StatusPending = "Pending";
-        public const string StatusApproved= "Approved";
+        public const string StatusApproved = "Approved";
         public const string StatusCheckedIn = "CheckedIn";
         public const string StatusCompleted = "Completed";
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
+
+        public static int VillaRoomsAvailable_Count(int villaId,
+             List<VillaNumber> villaNumberList, DateOnly checkInDate, int nights,
+            List<Booking> bookings)
+        {
+            List<int> bookingInDate = new();
+            int finalAvailableRoomForAllNigts = int.MaxValue;
+
+            var roomsInVilla = villaNumberList.Where(x => x.VillaId == villaId).Count();
+
+            for (int i = 0; i < nights; i++)
+            {
+                var villasBooked = bookings.Where(u => u.CheckInDate <= checkInDate.AddDays(i)
+                && u.CheckOutDate > checkInDate.AddDays(i) && u.VillaId == villaId);
+
+                foreach (var booking in villasBooked)
+                {
+                    if (!bookingInDate.Contains(booking.Id))
+                    {
+                        bookingInDate.Add(booking.Id);
+                    }
+                }
+
+                var totalAvailableRooms = roomsInVilla - bookingInDate.Count;
+                if (totalAvailableRooms == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (finalAvailableRoomForAllNigts > totalAvailableRooms)
+                    {
+                        finalAvailableRoomForAllNigts = totalAvailableRooms;
+                    }
+                }
+            }
+            return finalAvailableRoomForAllNigts;
+        }
     }
 }
